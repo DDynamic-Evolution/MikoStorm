887e417698b15aa975c8ab3f6ff7c961a72a3525
minerjr <minerjrminerjr@hotmail.com>
2026-06-21 21:34:05 -0300

    FIRE-36776 - Add omnifilter to the top status bar
    Added a new status bar panel to have 3 controls for the omnifitler.
    A check box to enable/disable the omnifilter
    A omnifilter icon button which will toggle the omnifilter rules editor visible/invisible.
    A drop down for the rule sets of the omnifilter.
    
    Created a class to handle the omnfiler control panel. Used to save having to modify the LL codebase.
    
    Omnifilter open while using the new UI features keeps the two in sync.

--- indra/newview/omnifilterengine.cpp
+++ indra/newview/omnifilterengine.cpp
@@ -591,6 +591,9 @@ S32 OmnifilterEngine::removeCurrentRuleSet()
             // Re-assign the current rule set name and reload the rule set from storage to the rule set
             assignRuleSetNameFromSettings();
             assignRuleSet();
+
+            // Signal all listeners that the rule sets have been updated
+            onRuleSetsUpdated();
             // Return 1 indicating that the removal worked correctly.
             return 1;
         }
@@ -629,6 +632,8 @@ S32 OmnifilterEngine::addNewRuleSet(std::string_view new_name)
     mCurrentSelectedRuleSet = str_new_name;
     // Add the rule set name to the list of ordered rule sets
     mOrderedRuleSets.push_back(mCurrentSelectedRuleSet);
+    // Signal all listeners that the rule sets have been updated
+    onRuleSetsUpdated();
 
     return 1;
 }
@@ -665,6 +670,8 @@ S32 OmnifilterEngine::addClonedRuleSet(std::string_view new_name)
     // Copy the internal rule set to the stored rulset
     assignRuleSet(false);
 
+    // Signal all listeners that the rule sets have been updated
+    onRuleSetsUpdated();
     return 1;
 }
 
@@ -929,4 +936,9 @@ bool OmnifilterEngine::tick()
     saveNeedles();
     setDirty(false);
     return false;
+}
+
+void OmnifilterEngine::onRuleSetsUpdated()
+{
+    mRuleSetUpdatedCallback();
 }
