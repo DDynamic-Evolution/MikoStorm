ced5123ba13f526ca762fe29c287f2ee76414d42
minerjr <minerjrminerjr@hotmail.com>
2026-06-29 09:25:29 -0300

    FIRE-36798 - Omnifilter - Add support for import/export rule set to Notecards
    Add new Export and Import button to Omnifilter window which will export and import the active rule set to a notecard.
    
    Export button once a name is entered, will open an inventory window with notecards only displayed and the new notecard highlighted.
    
    Update the Omnifilter Window to allow dragging and dropping of notecards onto the window to start the import process.
    
    The import button gives the user a prompt with instructions, as well, opens a new inventory window with notecard only being displayed.
    
    Notecard have description, and data stored as xml. with a header, name, version and actual rule set data.
    
    Added some new notification messages and error messages for the Export/Import feature.
    
    All new text supports being translated, by way of strings.xml, notifications.xml and floater_omnifilter.xml.
    
    Separated the import and export to LLSD methods to have rule set in a separate method from the all rule sets being exported and imported. So the rule set import/export method could be used by the new feature.

--- indra/newview/omnifilter.h
+++ indra/newview/omnifilter.h
@@ -50,6 +50,9 @@ private:
 public:
     bool              postBuild() override final;
     LLScrollListItem* addNeedle(const std::string& name, const OmnifilterEngine::Needle& needle);
+    // Supports Notecard drag and drop for importing.
+    bool handleDragAndDrop(S32 x, S32 y, MASK mask, bool drop, EDragAndDropType cargo_type, void* cargo_data, EAcceptance* accept,
+                           std::string& tooltip_msg);
 
 protected:
     OmnifilterEngine::Needle* getSelectedNeedle();
@@ -67,9 +70,14 @@ protected:
     void onNewRuleSetClicked();
     void onCloneRuleSetClicked();
     void onRemoveRuleSetClicked();
+    void onExportRuleSetClicked();
+    void onImportRuleSetClicked();
     void onNewRuleSetNameSelectedCallback(const LLSD& notification, const LLSD& response);
     void onCloneRuleSetNameSelectedCallback(const LLSD& notification, const LLSD& response);
     void onRemoveRuleSetConfirmedCallback(const LLSD& notification, const LLSD& response);
+    void onExportRuleSetConfirmedCallback(const LLSD& notification, const LLSD& response);
+    void onImportRuleSetConfirmedCallback(const LLSD& notification, const LLSD& response);
+    void onExportRuleSetNotecardCallback(const LLUUID &notecard_uuid);
     void onRuleSetChanged();
     void reloadRules();
     void reloadRule();
@@ -88,6 +96,8 @@ protected:
     LLButton*         mUpNeedleBtn{ nullptr };
     LLButton*         mDownNeedleBtn{ nullptr };
     // </FS:minerjr> [FIRE-36649]
+    LLButton*         mExportRuleSetBtn{ nullptr };
+    LLButton*         mImportRuleSetBtn{ nullptr };
     // <FS:minerjr> [FIRE-36763] - Add rule sets to Omnifilter
     LLComboBox* mRuleSetsCmb{ nullptr };
     LLButton* mNewRuleSetBtn{ nullptr };
