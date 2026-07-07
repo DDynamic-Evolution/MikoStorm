c599e205c5ac495b00a37ce689e6888514fe2a4d
minerjr <minerjrminerjr@hotmail.com>
2026-07-07 07:43:48 -0300

    FIRE-36835 - [Beta] Omni Filter doesn't seem to match regular expressions in dialogs and will crash on exclamation points
    Added a searchString method, which uses the regex::string as regex::match requires the entire string to match and does not support multiline content like search does.
    
    Also changed the regex::match to use ll_regex_match and used ll_regex_search for searchString method to match the rest of the codebase.
    
    Prevous code would miss all lines before the ! but would work for any after. With the search only on the content, rules that use regex now work correctly.
    
    Also, was unable to reproduce the crash. Tried to reproduce the conditions, but could not trigger.

--- indra/newview/omnifilterengine.h
+++ indra/newview/omnifilterengine.h
@@ -173,6 +173,7 @@ class OmnifilterEngine
     protected:
         const Needle* logMatch(const std::string& needle_name, const Needle& needle);
         bool matchStrings(std::string_view needle_string, std::string_view haystack_string, eMatchType match_type, bool case_insensitive);
+        bool searchStrings(std::string_view needle_string, std::string_view haystack_string, eMatchType match_type, bool case_insensitive);
 
         // <FS:minerjr> [FIRE-36763] - Add rule sets to Omnifilter
         bool importFromLLSD(const LLSD& data);
