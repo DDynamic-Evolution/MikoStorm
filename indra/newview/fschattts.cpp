603c5411cc7b55b22451dbeecf84eced7393a48b
Miko <miko@mikostorm>
2026-07-21 12:30:02 +0200

    Remove eSpeak-NG integration
    - Remove doEspeakTTS() dead code and espeak-ng include
    - Remove eSpeak settings from settings_per_account.xml
    - Update build command to use --no-espeak
    - TTS continues to work via external Voicebox server

--- indra/newview/fschattts.cpp
+++ indra/newview/fschattts.cpp
@@ -4,10 +4,7 @@
 
 #include "llagent.h"
 #include "llchat.h"
-#include "lldir.h"
-#include "llfile.h"
 #include "llframetimer.h"
-#include "llaudioengine.h"
 #include "llviewercontrol.h"
 #include "llcoros.h"
 #include "fscorehttputil.h"
@@ -16,10 +13,6 @@
 #include <queue>
 #include <vector>
 #include <boost/json.hpp>
-
-#ifdef LL_ESPEAK_NG
-# include <espeak-ng/speak_lib.h>
-#endif
 
 FSChatTTS* FSChatTTS::sInstance = nullptr;
 
@@ -30,9 +23,6 @@ FSChatTTS::FSChatTTS()
 
 FSChatTTS::~FSChatTTS()
 {
-#ifdef LL_ESPEAK_NG
-    espeak_Terminate();
-#endif
     sInstance = nullptr;
 }
 
@@ -209,55 +199,6 @@ void FSChatTTS::doTTS(const std::string& text)
     doVoiceboxTTS(text);
 }
 
-#ifdef LL_ESPEAK_NG
-
-static std::vector<short> sPcmBuffer;
-
-static int espeak_callback(short *wav, int numsamples, espeak_EVENT *events)
-{
-    if (wav == nullptr)
-        return 0;
-    sPcmBuffer.insert(sPcmBuffer.end(), wav, wav + numsamples);
-    return 0;
-}
-
-static bool init_espeak()
-{
-    std::string data_path;
-    std::string path;
-
-    path = gDirUtilp->getExecutableDir() + "/../espeak-ng-data";
-    LL_INFOS("TTS") << "Looking for espeak-ng-data at: " << path << LL_ENDL;
-    if (LLFile::isdir(path))
-        data_path = path;
-
-    if (data_path.empty())
-    {
-        path = gDirUtilp->getExecutableDir() + "/../../espeak-ng/espeak-ng-data";
-        LL_INFOS("TTS") << "Looking for espeak-ng-data at: " << path << LL_ENDL;
-        if (LLFile::isdir(path))
-            data_path = path;
-    }
-
-    LL_INFOS("TTS") << "espeak-ng-data path: " << (data_path.empty() ? "(default)" : data_path) << LL_ENDL;
-
-    int sample_rate = espeak_Initialize(AUDIO_OUTPUT_RETRIEVAL, 200,
-        data_path.empty() ? nullptr : data_path.c_str(),
-        espeakINITIALIZE_DONT_EXIT);
-    if (sample_rate < 0)
-    {
-        LL_WARNS("TTS") << "espeak_Initialize failed, sample_rate=" << sample_rate << LL_ENDL;
-        return false;
-    }
-
-    LL_INFOS("TTS") << "espeak_Initialize OK, sample_rate=" << sample_rate << LL_ENDL;
-
-    espeak_SetSynthCallback(espeak_callback);
-    return true;
-}
-
-#endif
-
 std::string FSChatTTS::jsonEscape(const std::string& text)
 {
     std::string result;
@@ -331,108 +272,4 @@ void FSChatTTS::doVoiceboxTTS(const std::string& text)
             LL_WARNS("TTS") << "Voicebox speak request failed" << LL_ENDL;
         },
         headers, opts);
-}
-
-void FSChatTTS::doEspeakTTS(const std::string& text)
-{
-#ifdef LL_ESPEAK_NG
-    static bool initialized = init_espeak();
-    if (!initialized)
-        return;
-
-    std::string voice = gSavedPerAccountSettings.getString("FSChatTTSEspeakVoice");
-    std::string variant = gSavedPerAccountSettings.getString("FSChatTTSEspeakVariant");
-    S32 speed = gSavedPerAccountSettings.getS32("FSChatTTSEspeakSpeed");
-    S32 pitch = gSavedPerAccountSettings.getS32("FSChatTTSEspeakPitch");
-    S32 amplitude = gSavedPerAccountSettings.getS32("FSChatTTSEspeakAmplitude");
-    S32 wordgap = gSavedPerAccountSettings.getS32("FSChatTTSEspeakWordGap");
-
-    {
-        std::string voice_name = voice;
-        if (!variant.empty())
-            voice_name = voice + "+" + variant;
-        espeak_ERROR voice_err = espeak_SetVoiceByName(voice_name.c_str());
-        if (voice_err != EE_OK)
-        {
-            LL_WARNS("TTS") << "espeak_SetVoiceByName(" << voice_name << ") failed: " << (int)voice_err << LL_ENDL;
-        }
-    }
-    espeak_SetParameter(espeakRATE, speed, 0);
-    espeak_SetParameter(espeakPITCH, pitch, 0);
-    espeak_SetParameter(espeakVOLUME, amplitude, 0);
-    espeak_SetParameter(espeakWORDGAP, wordgap, 0);
-
-    sPcmBuffer.clear();
-
-    unsigned int flags = espeakCHARS_UTF8;
-    espeak_ERROR synth_result = espeak_Synth(text.c_str(), text.size() + 1,
-        0, POS_CHARACTER, 0, flags, nullptr, nullptr);
-
-    if (synth_result != EE_OK)
-    {
-        LL_WARNS("TTS") << "espeak_Synth failed: " << (int)synth_result << LL_ENDL;
-        return;
-    }
-
-    espeak_Synchronize();
-
-    if (sPcmBuffer.empty())
-    {
-        LL_WARNS("TTS") << "No PCM data generated" << LL_ENDL;
-        return;
-    }
-
-    int sample_rate = 22050; // default espeak sample rate
-
-    LLUUID audio_uuid;
-    audio_uuid.generate();
-    std::string uuid_str;
-    audio_uuid.toString(uuid_str);
-    std::string cache_path = gDirUtilp->getExpandedFilename(LL_PATH_FS_SOUND_CACHE, uuid_str) + ".dsf";
-
-    {
-        llofstream outfile(cache_path, llofstream::binary);
-        if (!outfile.is_open())
-        {
-            LL_WARNS("TTS") << "Failed to open output file: " << cache_path << LL_ENDL;
-            return;
-        }
-
-        S32 data_size = (S32)(sPcmBuffer.size() * sizeof(short));
-        S32 file_size = 36 + data_size;
-        S16 audio_format = 1;
-        S16 num_channels = 1;
-        S32 byte_rate = sample_rate * num_channels * (S32)sizeof(short);
-        S16 block_align = num_channels * (S16)sizeof(short);
-        S16 bits_per_sample = 16;
-
-        outfile.write("RIFF", 4);
-        outfile.write((const char*)&file_size, 4);
-        outfile.write("WAVE", 4);
-        outfile.write("fmt ", 4);
-        S32 fmt_size = 16;
-        outfile.write((const char*)&fmt_size, 4);
-        outfile.write((const char*)&audio_format, 2);
-        outfile.write((const char*)&num_channels, 2);
-        outfile.write((const char*)&sample_rate, 4);
-        outfile.write((const char*)&byte_rate, 4);
-        outfile.write((const char*)&block_align, 2);
-        outfile.write((const char*)&bits_per_sample, 2);
-        outfile.write("data", 4);
-        outfile.write((const char*)&data_size, 4);
-        outfile.write((const char*)sPcmBuffer.data(), data_size);
-    }
-
-    if (gAudiop && gAudiop->preloadSound(audio_uuid))
-    {
-        gAudiop->triggerSound(audio_uuid, gAgent.getID(), 1.0f, LLAudioEngine::AUDIO_TYPE_TTS);
-        LL_INFOS("TTS") << "Playing eSpeak audio" << LL_ENDL;
-    }
-    else
-    {
-        LL_WARNS("TTS") << "Failed to preload eSpeak audio" << LL_ENDL;
-    }
-#else
-    LL_WARNS("TTS") << "eSpeak TTS not compiled in" << LL_ENDL;
-#endif
 }
